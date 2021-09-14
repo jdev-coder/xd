18292e24d9f74679d57b2ea8482ce614308923e3
jdev <72287871+jdev-coder@users.noreply.github.com>
2021-09-14 11:41:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -116,8 +116,7 @@ function fakealert(title,msg){
   */
   document.getElementById('close').style.fontFamily = 'sans-serif'
   document.getElementById('close').innerHTML = 'Close'
-  document.getElementById('notification').style.top = '50%'
-  document.getElementById('title').style.marginTop = '-25px'
+  document.getElementById('notification').style.height = '115px'
   document.getElementById('close').style.fontFamily = 'sans-serif'
   document.getElementById('close').style.fontSize = '14px'
   document.getElementById('title').style.visibility = 'hidden'
