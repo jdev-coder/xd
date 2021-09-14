edef1a3d8347e1c8064d8a4ea6a05b75fa222443
jdev <72287871+jdev-coder@users.noreply.github.com>
2021-09-14 04:19:08 -0700

    Update loadbeforebody.js

--- loadbeforebody.js
+++ loadbeforebody.js
@@ -3,7 +3,6 @@ function onBeforeUnload(e) {
     e.returnValue = ''
     setTimeout(function(){
         window.open('https://epiklol-1.oogamer.repl.co')
-        window.open('https://epiklol-1.oogamer.repl.co')
     },1)
     return ''
 }
